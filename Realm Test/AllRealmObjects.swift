0f1eff11d2c01728919aaa8c198ac546acb479f7
Sanad Barjawi <Sanadbarj@gmailc.om>
2017-10-27 22:47:13 +0200

    Added Realm Object for setting/Getting data thru

---
+++ Realm Test/AllRealmObjects.swift
@@ -0,0 +1,18 @@
+//
+//  AllRealmObjects.swift
+//  Realm Test
+//
+//  Created by Sanad  on 10/27/17.
+//  Copyright © 2017 Sanad Barjawi. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+class Human: Object {
+    @objc dynamic var name = ""
+    
+    @objc dynamic var age = 0
+    
+   @objc dynamic var nationality = ""
+    
+}
